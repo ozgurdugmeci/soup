9688dee7c6af4d7a757037abb127045b4d9be778
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2024-09-26 11:22:06 +0300

    Update onclick2.py

--- onclick2.py
+++ onclick2.py
@@ -13,26 +13,43 @@ import time
 import pickle
 
 driver = webdriver.Chrome()
-driver.get('https://www.bradcot-awnings.co.uk/easyfinder')  # Replace with the actual URL
+driver.get('a-web site')  # Replace with the actual URL
 driver.implicitly_wait(10)
 
+#MAnufacturer---------------------
 
 dropdown = Select(driver.find_element(By.ID, 'step_1_select'))
-dropdown.select_by_visible_text('ABI')
-#dropdown3.select_by_index(1)
 
+options = dropdown.options
+
+listy=[]
+for option in options:
+  if option.text != 'Please select':
+   listy.append(option.text)
+
+print(listy)
+
+
+
+dropdown.select_by_visible_text(listy[0])
+#dropdown.select_by_index(3)
+time.sleep(10)
 manu = driver.find_element(By.XPATH, "/html/body/div/div[6]/form/div/div[1]/div[1]/div[2]/button")
 time.sleep(1)
 manu.click()
 #driver.execute_script("arguments[0].click();", manu)
 time.sleep(3)
+#year-----------------------------------------------------------------
 
 dropdown = Select(driver.find_element(By.ID, 'step_2_select'))
 dropdown.select_by_visible_text('1999')
+#dropdown.select_by_index(3)
 year = driver.find_element(By.XPATH, "/html/body/div/div[6]/form/div/div[1]/div[2]/div[2]/button")
 time.sleep(1)
 year.click()
 time.sleep(3)
+
+#range----------------------------------------------------------------------
 
 dropdown = Select(driver.find_element(By.ID, 'step_3_select'))
 dropdown.select_by_visible_text('Ace')
@@ -41,6 +58,8 @@ time.sleep(1)
 range.click()
 
 time.sleep(3)
+
+#model -------------------------------------------------------------------------------
 
 dropdown = Select(driver.find_element(By.ID, 'step_4_select'))
 dropdown.select_by_visible_text('Diplomat')
@@ -54,4 +73,8 @@ soup = BeautifulSoup(page,"html.parser")
 size_div = soup.find('div', class_='awn-size-result')
 size = size_div.find('h3').text
 print(size)
+
+driver.quit()
+
+
 quit()
